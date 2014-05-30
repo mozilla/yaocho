4760b9e63edadf0ca7e9c1dbec50979b5d3db381
Mike Cooper <mythmon@gmail.com>
2014-05-30 14:43:33 -0700

    Clean up.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,21 +2,6 @@
 
 var yaocho = angular.module('yaocho');
 
-yaocho.controller('TopicListCtrl', ['$rootScope', '$scope', 'bindPromise', 'Kitsune', 'userprefs',
-function($rootScope, $scope, bindPromise, Kitsune, userprefs) {
-  $rootScope.$emit('title.change', userprefs.product.title + ' Support');
-  bindPromise($scope, 'topics', Kitsune.topics.all(userprefs.product.slug));
-}]);
-
-yaocho.controller('DocumentListCtrl', ['$rootScope', '$scope', '$routeParams', 'bindPromise', 'Kitsune', 'userprefs',
-function($rootScope, $scope, $routeParams, bindPromise, Kitsune, userprefs) {
-  bindPromise($scope, 'documents', Kitsune.documents.all({
-    product: userprefs.product.slug,
-    topic: $routeParams.topic,
-  }));
-  $rootScope.$emit('title.change', "All Document");
-}]);
-
 yaocho.controller('DocumentCtrl', ['$rootScope', '$scope', '$routeParams', 'bindPromise', 'Kitsune', 'safeApply',
 function($rootScope, $scope, $routeParams, bindPromise, Kitsune, safeApply) {
   bindPromise($scope, 'document', Kitsune.documents.get($routeParams.slug))
